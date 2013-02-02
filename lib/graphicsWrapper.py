4805072a80cd278b35f028c72bd53946d062fe9a
puddleworks <atalwai@puddleworks.com>
2013-02-02 16:35:55 -0500

    Merged Kyle's and Aaditya's wrapper

--- lib/graphicsWrapper.py
+++ lib/graphicsWrapper.py
@@ -23,6 +23,7 @@ class GraphicsWrapper():
             color = FillColor
         new_line = Line(self, (x1, y1), (x2, y2))
         window.draw(new_line)
+		return new_line
 
     def rect((x, y), rectWidth, rectHeight, color=None, filled=True, stroke=False):
         global FillColor
@@ -30,6 +31,7 @@ class GraphicsWrapper():
             color = FillColor
         new_rect = Rectangle(self, (x, y), rectWidth, rectHeight, color, true)
         window.draw(new_rect)
+		return new_rect
 
     def circle((x, y), radius, color=None, filled=True, stroke=False):
         global FillColor
@@ -37,6 +39,7 @@ class GraphicsWrapper():
             color = FillColor
         new_circle = Circle(self, (x, y), radius, FillColor, filled, stroke)
         window.draw(new_circle)
+		return new_circle
 
     def ellipse((x, y), width, height, color=None, filled=True, stroke=False):
         global FillColor
@@ -44,8 +47,15 @@ class GraphicsWrapper():
             color = FillColor
         new_ellipse = Ellipse(self, (x, y), width, height, color, filled)
         window.draw(new_ellipse)
+		return new_ellipse
 
-    def arc((x, y), width = 100, height = 100, startAngle = 0, endAngle = 180, color = None, filled = True, stroke = False):
+    def polygon (vertices, width, height, color = None, filled = True, stroke = False):
+		if color == None:
+			color = global FillColor
+		new_polygon = Polygon (self, vertices, color, filled, stroke)
+		return new_polygon
+	
+	def arc((x, y), width = 100, height = 100, startAngle = 0, endAngle = 180, color = None, filled = True, stroke = False):
         global FillColor
         if color == None:
             color = FillColor
@@ -72,58 +82,9 @@ class GraphicsWrapper():
             img._draw(window)
 
 
-#################################### Example #####################################
-
-# Can create static sketches by not using setup or draw and putting code in "main"
-drawLine((x1, y1), (x2, y2))
-drawRect((x, y), rectWidth, rectHeight)
-drawPolygon((x1, y1), (x2, y2), (x3, y3))
-# What if have code in "main" and have setup and draw methods
-
-setSize(300, 400)  # sets window size
-setFrameRate(35)  # Can be set or not (default)
-setStroke(red)  # Sets line drawing color to red
-setStroke(125)  # Sets line drawing color to (125, 125, 125) (easy for white/grays/black)
-setStroke(126, 35, 92)  # Sets line drawing color to (126, 35, 92)
-
-noLoop()    # Causes draw to not be looped
-loop()      # Causes draw to be looped (default?)
-redraw()    # Calls draw once; useful for events
-
-
-# Gets repeatedly called until program ends (window closes?)
-def draw():
-    setBackground(blue)
-
-    # height and width are global variables and are from setSize()
-    y = height
-    x = width
-
-    drawLine((x1, y1), (x2, y2))
-    drawRect((x, y), rectwidth, rectheight)
-    # etc.
-
-    if (mousePressed):
-        setStroke(255)
-    else:
-        setStroke(0)
-
-    # Takes all the color definitions like setStroke
-    setFill()
-    # If drawFilledRect() is called with different stroke and fill colors,
-
-    # mouseX and mouseY are the current mouse coordinates
-    drawLine(mouseX - 66, mouseY, mouseX + 66, mouseY)
-    drawLine(mouseX, mouseY - 66, mouseX, mouseY + 66)
-
-    img = loadImage('path')
-    drawImage(img, (x, y), imageWidth, imageHeight)
-    drawImage(img, (x, y))  # Draws image at the original width and height
-
-
-def mousePressed():
-    # Perform some magic
-    drawRect((x, y), mouseX, mouseY)
+	def mousePressed():
+		# Perform some magic
+		drawRect((x, y), mouseX, mouseY)
 
 drawFunction(draw)
 mousePressedFunction(mousePressed)
