9fd273dc86467319d0ee78cc766ed65123dcb5c6
Janet Kim <janetthekim@dhcp-210-234.cs.dartmouth.edu>
2013-02-03 15:01:57 -0500

    Added KeyListeners and MouseListener methods

--- lib/GraphicsWindow.py
+++ lib/GraphicsWindow.py
@@ -1,6 +1,7 @@
 from java.awt.event import ActionListener, KeyListener, MouseListener
 from java.awt.Color import * # so we can just say gray instead of Color.gray
 from javax.swing import JFrame, JPanel
+from javax.swing.event import MouseInputListener
 from java.lang import Math
 from Image import *
 from Group import *
@@ -12,8 +13,8 @@ from Text import *
 # class Components:
 
 
-class GraphicsWindow(ActionListener, KeyListener, MouseListener):
-    
+class GraphicsWindow(ActionListener, KeyListener, MouseInputListener):
+    #we need motion input using MouseInputListener
     def __init__(self, title, width, height, backgroundColor = white):
         assert width > 0, "GraphicsWindow width must be greater than zero"
         assert height > 0, "GraphicsWindow height must be greater than zero"
@@ -23,6 +24,20 @@ class GraphicsWindow(ActionListener, KeyListener, MouseListener):
         self.frame = JFrame(title,defaultCloseOperation = JFrame.EXIT_ON_CLOSE,
                             size = (self.width, self.height))
         self.frame.contentPane = Canvas(self, self.objs, backgroundColor)
+        self.frame.addMouseListener(self) 
+        self.frame.addMouseMotionListener(self)
+        self.frame.addKeyListener(self)
+        self.mouseX = 0
+        self.mouseY = 0
+        
+        #MouseEvent booleans mousePressed, mouseClicked, mouseDragged
+        self.mouseP = False
+        self.mouseC = False
+        self.mouseD = False
+    
+        #KeyEvent booleans keyPressed, keyTyped
+        self.keyP = False
+        self.keyT = False
     
     def setVisible(self, isVisible):
         self.frame.visible = isVisible
@@ -44,7 +59,47 @@ class GraphicsWindow(ActionListener, KeyListener, MouseListener):
     def setDefaultColor(self, c):
         self.frame.contentPane.setDefaultColor(c)
 
-class Canvas(JPanel, ActionListener, KeyListener):
+    #We put these mouse location methods in the window class in case we implement multiple panels    
+    #MouseListener methods
+    def mouseEntered (self, e):
+        self.mouseX = e.getXOnScreen()
+        self.mouseY = e.getYOnScreen()
+        print self.mouseX
+        print self.mouseY
+    def mouseX (self):
+        return self.mouseX    
+    def mouseY (self):
+        return self.mouseY
+    def mouseClicked (self,e):
+        self.mouseC = True
+    def mouseExited (self,e):
+        pass
+    def mousePressed (self,e):
+        self.mouseP = True
+    def mouseReleased (self,e):
+        self.mouseP = False
+        self.mouseC = False
+        self.mouseD = False
+    def mouseMoved (self,e):
+        self.mouseX = e.getXOnScreen()
+        self.mouseY = e.getYOnScreen()
+        print self.mouseX
+        print self.mouseY
+    def mouseDragged (self,e):
+        self.mouseD = True
+        
+    #KeyListener methods
+    def keyTyped (self,e):
+        self.keyT = True
+        print e.getKeyChar()
+    def keyPressed (self,e):
+        self.keyP = True
+        print e.getKeyChar()
+    def keyReleased (self,e):
+        self.keyT = False
+        self.keyP = False
+
+class Canvas(JPanel):
     """ Canvas to draw the action on. Owns the action and key listeners. """
     
     def __init__(self, window, objects, backgroundColor):
@@ -66,15 +121,8 @@ class Canvas(JPanel, ActionListener, KeyListener):
             else:
                 g.setColor(self.objs[i].getColor())
             self.objs[i]._draw(g)
-    
-    def actionPerformed(self, a):
-        pass
-    
-    def keyReleased(self, e):
-        pass
-    
-    def keyPressed(self, e):
-        pass
-    
+
     def setDefaultColor(self, c):
         self.defaultColor = c
+
+
