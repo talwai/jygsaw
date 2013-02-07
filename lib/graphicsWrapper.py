4ba89a71873b836955fd382665e2c89225b82842
Kyle Lawson <kylelawson23@gmail.com>
2013-02-07 10:17:43 -0500

    got rid of tuples

--- lib/graphicsWrapper.py
+++ lib/graphicsWrapper.py
@@ -20,6 +20,7 @@ Creates a new window. Width, height and title can be set optionally as well
 def canvas(width = 400, height = 400, window_title = ' ', background=white):
     global window
     window = GraphicsWindow(window_title, width, height, background)
+    window.setVisible(True)
 
 
 """
@@ -37,7 +38,7 @@ def height():
 """
 Draws a line between coordinates (x1, y1), and (x2 and y2). You can optionally set the color as well
 """
-def line((x1, y1), (x2, y2), color=None):
+def line(x1, y1, x2, y2, color=None):
     new_line = Line((x1, y1), (x2, y2))
     window.draw(new_line)
     return new_line
@@ -45,7 +46,7 @@ def line((x1, y1), (x2, y2), color=None):
 """
 Creates a rectangle with the upper left corner at the given (x,y) coordinates.
 """
-def rect((x, y), rectWidth = 100, rectHeight = 100, color=None, filled=True, stroke=Stroke):
+def rect(x, y, rectWidth = 100, rectHeight = 100, color=None, filled=True, stroke=Stroke):
     new_rect = Rectangle((x, y), rectWidth, rectHeight, color, filled)
     window.draw(new_rect)
     return new_rect
@@ -54,7 +55,7 @@ def rect((x, y), rectWidth = 100, rectHeight = 100, color=None, filled=True, str
 Creates a circle centered at the given (x,y) coordinates. The radius, color, filled status, and stoke status can be
 optionally modified.
 """
-def circle((x, y), radius = 50, color=None, filled=True, stroke=Stroke):
+def circle(x, y, radius = 50, color=None, filled=True, stroke=Stroke):
     new_circle = Circle((x, y), radius, color, filled)
     window.draw(new_circle)
     return new_circle
@@ -63,7 +64,7 @@ def circle((x, y), radius = 50, color=None, filled=True, stroke=Stroke):
 Creates an eclipse centered at the given (x,y) coordinates. Width, height, color, filled status and stroke status
 can be optionally modified.
 """
-def ellipse((x, y), width = 100, height = 100, color=None, filled=True, stroke=Stroke):
+def ellipse(x, y, width = 100, height = 100, color=None, filled=True, stroke=Stroke):
     new_ellipse = Ellipse((x, y), width, height, color, filled)
     window.draw(new_ellipse)
     return new_ellipse
@@ -72,7 +73,7 @@ def ellipse((x, y), width = 100, height = 100, color=None, filled=True, stroke=S
 Creates a polygon whose points are given in a list as the first argument. Width, height, color, filled status and stroke status can
 be optionally modified
 """
-def polygon (vertices, color=None, filled=True, stroke=Stroke):
+def polygon(vertices, color=None, filled=True, stroke=Stroke):
     new_polygon = Polygon (vertices, color, filled)
     window.draw(new_polygon)
     return new_polygon
@@ -81,7 +82,7 @@ def polygon (vertices, color=None, filled=True, stroke=Stroke):
 Creates an arc centered at the given (x,y) coordinates. The width, heigh, start angle, end angle, color, filled
 status and stoke status can be optionally modified. Start angle and end angle refer to the 
 """
-def arc((x, y), width = 100, height = 100, startAngle = 0, endAngle = 180, color=None, filled=True, stroke=Stroke):
+def arc(x, y, width = 100, height = 100, startAngle = 0, endAngle = 180, color=None, filled=True, stroke=Stroke):
     new_arc = Arc((x,y), width, height, startAngle, (endAngle - startAngle), color)
     window.draw(new_arc)
     return new_arc
@@ -93,10 +94,11 @@ def arc((x, y), width = 100, height = 100, startAngle = 0, endAngle = 180, color
 Draws an image with upper left corner at the given (x,y) coordinates. The image should be located at imagePath,
 and the desired width and height of the image should be specified in their respective arguments.
 """
-def image((x, y), imagePath, width, height):
+def image(x, y, imagePath, width, height):
     global window
     img = Image((x, y), imagePath, width, height)
     window.draw(img)
+    return img
 
 """
 Sets the color to fill shapes with.
@@ -110,7 +112,6 @@ Sets the background color of the window.
 def setBackground(color):
     window.setBackgroundColor(color)
     # It would be nice to be able to accept multiple types of color input (r,g,b or name of color)
-    # Doesn't seem to work
 
 def mouseX():
     return window.mouseX()
@@ -127,11 +128,8 @@ def stroke():
     Stroke = True
 
 """
-Draws all of the objects on the window
+Redraws all of the objects on the window
 """
-def draw():
-    window.setVisible(True)
-
 def redraw():
     window.redraw()
 
@@ -142,23 +140,22 @@ if ( __name__ == '__main__' ) or ( __name__ == 'main' ):
     
     fill(red)
     
-    rect((10,10))
+    rect(10,10)
     
-    line((150,10),(200,10))
+    line(150, 10, 200, 10)
 
     fill(pink)
     
-    ellipse((10,150))
+    ellipse(10, 150)
 
     print width(), height()
     
     vertices = [(250,250),(360,360), (360, 250)]
+    
     polygon(vertices)
     
-    #arc((250, 250))
+    #arc(250, 250)
         
-    #circle((10,110))
+    #circle(10,110)
 
     setBackground(black)
-    
-    draw()
