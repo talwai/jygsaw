94470340565564f5d29cd4409a433509708ba95c
Jennifer Lure <jennifer.c.lure.13@dartmouth.edu>
2013-02-10 21:04:21 +0000

    recursion.py now works

--- demos/recursion.py
+++ demos/recursion.py
@@ -5,14 +5,19 @@ from graphicsWrapper import *
 
 
 def draw():
-    drawCircle(width / 2, 280, 6)
+    drawCircle(width() / 2, 280, 6)
+    # The redraw is necessary, otherwise window may be shown before
+    # all the objects are drawn.
+    redraw()
+    
 
 
 # Recursively draw smaller circles
 def drawCircle(x, radius, level):
-    tt = 126 * level / 4.0
-    fillColor(tt)
-    ellipse(x, height / 2, radius * 2, radius * 2)
+    tt = int(126 * level / 4.0)
+    fill(tt)
+    circle(x, height() / 2, radius)
+    #ellipse(x, height() / 2, radius * 2, radius * 2)
     if (level > 1):
         level = level - 1
         drawCircle(x - radius / 2, radius / 2, level)
@@ -23,4 +28,4 @@ canvas(640, 360)
 noStroke()
 noLoop()
 
-drawFunction(draw)
+onDraw(draw)
