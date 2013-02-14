c1385c2b785e46e1d4f9a7d429b2c6150b9f3718
Jennifer Lure <jennifer.c.lure.13@dartmouth.edu>
2013-02-14 17:11:47 +0000

    changed frame rate to floats, reduces flickering

--- lib/graphicsWrapper.py
+++ lib/graphicsWrapper.py
@@ -10,7 +10,7 @@ import time
 
 toLoop = False
 Stroke = False
-fr = 60 # Frame Rate
+fr = 60.0 # Frame Rate
 
 def canvas(width=400, height=400, window_title=' ', background=white):
     """
@@ -270,7 +270,7 @@ def frameRate(rate):
     """
 
     global fr
-    fr = rate
+    fr = float(rate)
 
 def stroke(r = None, g = None, b = None):
     """
@@ -305,7 +305,7 @@ def onDraw(draw):
         while toLoop:
             draw()
             redraw()
-            time.sleep(1/fr)
+            time.sleep(1.0/fr)
     else:
         draw()
         redraw()
