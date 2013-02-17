f7bc0456aeb7f53bdc5b98ff95ae19f47b2f2aea
Janet Kim <janetthekim@dhcp-210-72.cs.dartmouth.edu>
2013-02-17 18:49:25 -0500

    Updated tutorial. Wrote demos tutorialShapes and tutorialPointsLines

---
+++ demos/tutorialPointsLines.py
@@ -0,0 +1,20 @@
+import sys
+import os
+sys.path.append(os.path.pardir + "/lib")
+from graphicsWrapper import *
+from random import random, choice
+
+canvas(900,500)
+background(darkGray)
+
+#	def point(x, y, color=None):
+
+def draw():
+	lineX = random()*800 +50
+	lineY = random()*300 +125 
+	lineColor = choice([lightGray,white,orange, gray])
+	line(width()/2,height(),lineX,lineY,color=lineColor)
+	point(lineX, lineY -100, color=lineColor)
+	loop()
+
+onDraw(draw)
