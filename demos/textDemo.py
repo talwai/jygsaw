16a35cb74461d97f3b7fc1159f5abf5b2bb16b6e
Janet Kim <janetthekim@dhcp-210-234.cs.dartmouth.edu>
2013-02-23 16:29:02 -0500

    new text demo, doesn't wrap but processes return lines

---
+++ demos/textDemo.py
@@ -0,0 +1,31 @@
+from jygsaw.graphics import *
+
+canvas(750, 360)
+background(darkGray)
+loop()
+words = ""
+letterWidth = 20
+y = 60
+tH = 30
+
+def draw():
+    text((25, 25), "Type onto the screen:", "Georgia", 25, color=lightGray, attribute=PLAIN)
+    text((25, y), words, "Arial", tH, color=white, attribute=PLAIN)
+
+   
+def keyPressed():
+    global words, y, tH
+    # If the key is between 'A' (65) and 'z' (122)
+    k = lastKeyChar()
+    c = lastKeyCode()
+    if (c!=10 and c!=16):
+        print k
+        print c
+        words += k
+    elif c==10:
+        y+=tH
+        words=""
+
+
+onKeyPress(keyPressed)
+onDraw(draw)
