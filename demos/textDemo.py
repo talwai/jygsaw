787f72b5303bd008a24930b1bced2ce9b859be5f
Janet Kim <janetthekim@dhcp-210-234.cs.dartmouth.edu>
2013-02-24 15:28:59 -0500

    updated textDemo reflecting text.py changes

--- demos/textDemo.py
+++ demos/textDemo.py
@@ -3,7 +3,7 @@ from jygsaw.graphics import *
 
 
 canvas(750, 360)
-background(white)
+background(darkGray)
 loop()
 y = 60
 textHeight = 30
@@ -13,20 +13,22 @@ textList=[]
 def draw():
     global y,textHeight, words
     clear()
-    text((25, 25), "Type onto the screen:", "Georgia", 25, color=lightGray, attribute=PLAIN)
+    t=text(25, 25, "Type onto the screen:", color=gray, attribute=PLAIN)
+    t._set_size(textHeight)
+    t._set_font("Georgia")
     for (i,h) in textList:
-        text((25, h), i, "Arial", textHeight, color=white, attribute=PLAIN)
-    text((25, y), words, "Arial", textHeight, color=white, attribute=PLAIN)
-    
+        ti=text(25, h, i, color=white, attribute=PLAIN)
+        ti._set_font("Arial")
+        ti._set_size(textHeight)
+    tw=text(25, y, words, color=white, attribute=PLAIN)
+    tw._set_font("Arial")
+    tw._set_size(textHeight)
 
 def keyPressed():
     global words, textList, y, textHeight
-    # If the key is between 'A' (65) and 'z' (122)
     k = lastKeyChar()
     c = lastKeyCode()
     if (c!=10 and c!=16):
-        print k
-        print c
         words += k
     elif c==10:
         newLine=words
