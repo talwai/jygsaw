6ef419e57430fe25fc95fcc8ccb7db5f699eb8ef
Avery Yen <haplesshero13@gmail.com>
2013-02-24 20:45:58 -0500

    Fixed textDemo style

--- demos/textDemo.py
+++ demos/textDemo.py
@@ -1,7 +1,6 @@
 from jygsaw.graphics import *
 
 
-
 canvas(750, 360)
 background(white)
 loop()
@@ -10,24 +9,25 @@ letterWidth = 20
 y = 60
 tH = 30
 
+
 def draw():
     clear()
     text((25, 25), "Type onto the screen:", "Georgia", 25, color=lightGray, attribute=PLAIN)
     text((25, y), words, "Arial", tH, color=black, attribute=PLAIN)
 
-   
+
 def keyPressed():
     global words, y, tH
     # If the key is between 'A' (65) and 'z' (122)
     k = lastKeyChar()
     c = lastKeyCode()
-    if (c!=10 and c!=16):
+    if (c != 10 and c != 16):
         print k
         print c
         words += k
-    elif c==10:
-        y+=tH
-        words=""
+    elif c == 10:
+        y += tH
+        words = ""
 
 
 onKeyPress(keyPressed)
