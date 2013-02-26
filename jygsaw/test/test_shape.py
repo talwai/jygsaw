50b5e9dbd1e7c3816e9635ea7d5dc9972e2db2bb
Avery Yen <haplesshero13@gmail.com>
2013-02-26 16:27:40 -0500

    Jay kay fixed even more tabs and formatting issues

--- jygsaw/test/test_shape.py
+++ jygsaw/test/test_shape.py
@@ -2,6 +2,7 @@ from jygsaw.graphics import *
 
 import unittest
 
+
 class TestShapeFunctions(unittest.TestCase):
 
     def setUp(self):
@@ -10,28 +11,25 @@ class TestShapeFunctions(unittest.TestCase):
         stroke()
         frameRate(60.0)
 
-	self.arc = arc(300,100,123,33,34,33)
+        self.arc = arc(300, 100, 123, 33, 34, 33)
 
-        self.ellipse = ellipse(10, 150,40, 40)
+        self.ellipse = ellipse(10, 150, 40, 40)
 
         self.polygon = polygon(
             [(250, 250), (250, 370), (360, 340), (360, 250)])
-	self.regPolygon = regPolygon(10, 300,40,40)
-	
+        self.regPolygon = regPolygon(10, 300, 40, 40)
 
     def test_arc(self):
-		
-	self.assertEqual(self.arc._get_color(),color(128,128,128))
+        self.assertEqual(self.arc._get_color(), color(128, 128, 128))
 
+        fill(0, 255, 0)
+        self.arc = arc(300, 100, 123, 33, 34, 33)
 
-	fill(0,255,0)
-	self.arc = arc(300,100,123,33,34,33) 
+        self.assertEqual(self.arc._get_filled, True)
+        self.assertEqual(self.arc._filled, green)
 
-	self.assertEqual(self.arc._get_filled,True)
-	self.assertEqual(self.arc._filled,green)
+        self.assertEqual(self.arc._get_color(), green)
 
-	self.assertEqual(self.arc._get_color(),green)
-	    
         self.assertEqual(self.arc._get_width(), 123)
         self.assertEqual(self.arc._get_height(), 33)
 
@@ -40,7 +38,6 @@ class TestShapeFunctions(unittest.TestCase):
 
         self.arc._set_height(170)
         self.assertEqual(self.arc._get_height(), 170)
-	
 
     def test_polygon(self):
         self.assertEqual(self.polygon._get_vertices(), [(250, 250), (250,
