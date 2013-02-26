3d99fbbdd206b8d97259532bb7812af835d50920
Jennifer Lure <jennifer.c.lure.13@dartmouth.edu>
2013-02-26 16:51:46 +0000

    fixed imports and default values for mouse callbacks

--- jygsaw/graphicswindow.py
+++ jygsaw/graphicswindow.py
@@ -5,8 +5,8 @@ GraphicsWindow on window creation. A window holds the drawing canvas.
 
 from __future__ import with_statement
 from java.awt.event import ActionListener, KeyListener, MouseListener, MouseEvent, KeyEvent, ActionEvent
-from java.awt import Dimension, RenderingHints
-from java.awt.Color import *  # so we can just say gray instead of Color.gray
+from java.awt import Color, Dimension, RenderingHints
+from java.awt.Color import black, blue, cyan, darkGray, gray, green, lightGray, magenta, orange, pink, red, white, yellow
 from javax.swing import JFrame, JPanel
 from javax.swing.event import MouseInputListener
 from image import *
@@ -62,6 +62,8 @@ class GraphicsWindow(ActionListener, KeyListener, MouseInputListener):
         self.onMousePressed = None
         self.onMouseReleased = None
         self.onMouseDragged = None
+        self.onMouseExited = None
+        self.onMouseEntered = None
 
         # KeyEvent booleans keyPressed, keyTyped
         self.keyEventType = 0
@@ -156,7 +158,7 @@ class GraphicsWindow(ActionListener, KeyListener, MouseInputListener):
         """
         Redraws the Canvas; only returns when done. An optional float
         can also be used to sleep after redrawing.
-        """        
+        """
         self.frame.contentPane.blocking_redraw()
         sleep(delay)
 
