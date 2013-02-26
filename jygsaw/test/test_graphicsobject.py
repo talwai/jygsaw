364e976645e560f76db34aad2929fc34729499e7
Aaditya Talwai <aaditya.21@gmail.com>
2013-02-26 11:18:06 -0500

    updates test graphicsobject

---
+++ jygsaw/test/test_graphicsobject.py
@@ -0,0 +1,59 @@
+from jygsaw.graphics import *
+
+import unittest
+
+class TestShapeFunctions(unittest.TestCase):
+
+    def setUp(self):
+        canvas()
+        loop()
+        stroke()
+        frameRate(60.0)
+
+	self.arc = arc(-300,-100,123,33,34,33)
+
+        self.ellipse = ellipse(10, 150,40, 40)
+
+        self.polygon = polygon(
+            [(250, 250), (250, 370), (360, 340), (360, 250)])
+	self.regPolygon = regPolygon(10, 300,40,40)
+	
+
+    def test_arc(self):
+		
+	self.assertEqual(self.arc._get_color(),color(128,128,128))
+
+
+	fill(0,255,0)
+	self.arc = arc(300,100,123,33,34,33) 
+
+	self.assertEqual(self.arc._get_color(),green)
+	    
+        self.assertEqual(self.arc._get_x(), 300)
+        self.assertEqual(self.arc._get_y(), 100)
+
+        self.arc._set_x(130)
+        self.assertEqual(self.arc._get_x(), 130)
+
+        self.arc._set_y(170)
+        self.assertEqual(self.arc._get_y(), 170)
+	
+	c = color(255,255,255)
+	
+	self.arc._set_color(c)
+	self.assertEqual(self.arc._get_color(),c)
+
+	self.arc.moveTo(1500,1500)
+	self.assertEqual(self.arc._get_x(),1500)
+	self.assertEqual(self.arc._get_y(),1500)
+	
+	self.arc.move(-1600,-1600)
+	self.assertEqual(self.arc._get_x(),-100)
+	self.assertEqual(self.arc._get_y(),-100)
+
+    def test_polygon(self):
+        self.assertEqual(self.polygon._get_vertices(), [(250, 250), (250,
+                         370), (360, 340), (360, 250)])
+
+if (__name__ == '__main__') or (__name__ == 'main'):
+    unittest.main()
