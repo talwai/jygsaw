b70a714b99b5dcda4f9d4e624d8327e9463acb5c
Devin Balkcom <devin@cs.dartmouth.edu>
2013-02-28 11:48:56 -0500

    added comments to pastel circles demo

--- demos/pastel_circles.py
+++ demos/pastel_circles.py
@@ -1,20 +1,27 @@
+# pastel circles demo of jygsaw
+
 from jygsaw.graphics import *
 from random import randint
 
 canvas()
-background(0, 0, 0)
+background(black)
 
 while True:
 
+	# clear screen with nearly-transparent black rectangle
+	fill(0, 0, 0, 15)
+	rect(0, 0, 399, 399)
+
+	# random location for circle
 	x = randint(0, 400)
 	y = randint(0, 400)
 
+	# random color for circle
 	r = randint(128, 255)
 	g = randint(128, 255)
 	b = randint(128, 255)
 
 	fill(r, g, b)
-
 	circle(x, y, 50)
 
-	redraw(.05)
+	redraw(.02)
