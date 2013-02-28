b562896adcf6904160ac79fb335c035e3029f395
Janet Kim <janetthekim@dhcp-210-233.cs.dartmouth.edu>
2013-02-28 11:03:52 -0500

    updated textDemo

--- demos/textDemo.py
+++ demos/textDemo.py
@@ -13,18 +13,14 @@ def draw():
     global currentLineHeight, textHeight, words
     clear()
 
-    t = text(25, 25, "Type onto the screen:", color=gray, attribute=PLAIN)
-    t._set_size(textHeight)
-    t._set_font("Georgia")
-
+    textSize(textHeight)
+    font("Georgia")
+    t = text(25, 25, "Type onto the screen:", color=gray)
+    font("Arial")
     for (i, h) in textList:
-        ti = text(25, h, i, color=white, attribute=PLAIN)
-        ti._set_font("Arial")
-        ti._set_size(textHeight)
+        ti = text(25, h, i, color=white)
 
-    tw = text(25, currentLineHeight, words, color=white, attribute=PLAIN)
-    tw._set_font("Arial")
-    tw._set_size(textHeight)
+    tw = text(25, currentLineHeight, words, color=white)
 
 
 def keyPressed():
