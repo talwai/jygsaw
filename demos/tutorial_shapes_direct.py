4cb89c606a23088e247192710b0ae83de59fc363
Jennifer Lure <jennifer.c.lure.13@dartmouth.edu>
2013-05-05 23:09:48 +0100

    added direct and managed mode for tutorial_shapes and tutorial_points_lines

---
+++ demos/tutorial_shapes_direct.py
@@ -0,0 +1,18 @@
+# tutorial_shapes_direct.py
+#
+# Jygsaw demo - draws some shapes
+#
+# Written by Janet Kim for Jygsaw
+
+from jygsaw.graphics import *
+
+canvas(700, 300)
+background(blue)
+
+
+circle(350, 150, 50, color=red)
+point(350, 150)
+rect(0, 0, 150, 75, color=orange)
+polygon([(450, 250), (650, 250), (500, 300)], color=yellow)
+ellipse(150, 75, 100, 250, color=green)
+regPolygon(550, 85, sides=6, length=80, color=magenta)
